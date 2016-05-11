12483cb6b700d1fc1df2244758ef2494dd031475
Mohamed el fadhel el hachemi <mohamedelfadhel.elhachemi@esprit.tn>
2016-05-11 20:04:33 +0100

    added forum bans

--- Tesseract-Symfony/src/Tesseract/MOOCBundle/Controller/ForumBansController.php
+++ Tesseract-Symfony/src/Tesseract/MOOCBundle/Controller/ForumBansController.php
@@ -7,18 +7,29 @@
  */
 
 namespace Tesseract\MOOCBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 /**
  * Description of ApprenantController
  *
  * 
  */
-
 class ForumBansController extends Controller {
+
     public function indexAction() {
-       
-        return $this->render("TesseractMOOCBundle:Admin:ForumBans.html.twig");
+        $em = $this->getDoctrine()->getManager();
         
         
+        $query = $em->createQuery('Select s,c  from TesseractMOOCBundle:CommentaireSujet c join c.idSujet s ');
+        $suj_com=$query->getResult();
+        $notifications = $em->getRepository("TesseractMOOCBundle:Notification")->findBy(array('idUtilisateur' => $this->getUser()->getId(),
+            'vue' => 'non'));
+
+        $nbr = count($notifications);
+
+        return $this->render("TesseractMOOCBundle:Admin:ForumBans.html.twig",array('suj_com'=>$suj_com, 'notifications'=>$notifications,
+                                                                                        'nbrnot'=>$nbr));
     }
+
 }
--- Tesseract-Symfony/src/Tesseract/MOOCBundle/Controller/OrganisationQueriesController.php
+++ Tesseract-Symfony/src/Tesseract/MOOCBundle/Controller/OrganisationQueriesController.php
@@ -17,12 +17,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class OrganisationQueriesController extends Controller {
     public function indexAction() {
          $em = $this->getDoctrine()->getManager();
-        $orgs = $em->getRepository("TesseractMOOCBundle:Organisation")->findBy(array('enabled' => '0'));
+        $orgs = $em->getRepository("TesseractMOOCBundle:Organisation")->findBy(array('enabled' => '-1'));
         $notifications = $em->getRepository("TesseractMOOCBundle:Notification")->findBy(array('idUtilisateur' =>$this->getUser()->getId(),
                                                                                            'vue'=>'non' ));
         
         $nbr=count($notifications);
        
+        return $this->render("TesseractMOOCBundle:Admin:OrganisationQueries.html.twig",array('orgs'=> $orgs ,
+                                                                                            'notifications'=>$notifications,
+                                                                                        'nbrnot'=>$nbr));
+        
+    }
+    public function replyAction($reply,$id) {
+         $em = $this->getDoctrine()->getManager();
+        $orgs = $em->getRepository("TesseractMOOCBundle:Organisation")->findBy(array('enabled' => '-1'));
+        $notifications = $em->getRepository("TesseractMOOCBundle:Notification")->findBy(array('idUtilisateur' =>$this->getUser()->getId(),
+                                                                                           'vue'=>'non' ));
+        
+        $nbr=count($notifications);
+         if($reply == 'Accept'){
+            $org=$em->getRepository("TesseractMOOCBundle:Organisation")->find($id);
+            $org->setEnabled(1);
+            $em->persist($org);
+            $em->flush();
+        }
+        if($reply == 'Refuse'){
+            $org=$em->getRepository("TesseractMOOCBundle:Organisation")->find($id);
+            $org->setEnabled(0);
+            $em->persist($org);
+            $em->flush();
+        }
+       $orgs = $em->getRepository("TesseractMOOCBundle:Organisation")->findBy(array('enabled' => '-1'));
         return $this->render("TesseractMOOCBundle:Admin:OrganisationQueries.html.twig",array('orgs'=> $orgs ,
                                                                                             'notifications'=>$notifications,
                                                                                         'nbrnot'=>$nbr));
